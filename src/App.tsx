85f22156ffb0a584d6a8ea699dfe2ffc1f02ba56
Pavel Schoffer <xschof00@gmail.com>
2022-08-24 11:55:39 +0200

    bootstrap repo

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div >
+      <header className="m-2">
+        EgoBlock
+      </header>
+    </div>
+  );
+}
+
+export default App;
