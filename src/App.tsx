3ebbca75a925fccec3b910641a3442429f9cc263
Pavel Schoffer <xschof00@gmail.com>
2022-08-26 20:58:40 +0200

    baseWallet

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,49 @@
 import React from 'react';
+import { chain, configureChains, createClient, WagmiConfig } from 'wagmi';
+import { publicProvider } from 'wagmi/providers/public'
 import './App.scss';
+import WalletLoginPage from './pages/WalletLoginPage';
+import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet'
+import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
+import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
+
+const { chains, provider, webSocketProvider } = configureChains(
+  [chain.mainnet, chain.polygon],
+  [publicProvider()],
+)
+
+const client = createClient({
+  autoConnect: true,
+  provider,
+  webSocketProvider,
+  connectors: [
+    new MetaMaskConnector({ chains }),
+    new CoinbaseWalletConnector({
+      chains,
+      options: {
+        appName: 'Ego Block',
+      },
+    }),
+    new WalletConnectConnector({
+      chains,
+      options: {
+        qrcode: true,
+      },
+    }),
+  ],
+
+})
 
 function App() {
   return (
-    <div >
-      <header className="m-2">
-        EgoBlock
-      </header>
-    </div>
+    <WagmiConfig client={client}>
+      <div >
+        <header className="m-2">
+          EgoBlock
+        </header>
+        <WalletLoginPage />
+      </div>
+    </WagmiConfig>
   );
 }
 
