91307d383dfa5570d28f43c3b87d954163dec966
Pavel Schoffer <xschof00@gmail.com>
2022-08-27 15:47:13 +0200

    base auth

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ import { chain, configureChains, createClient, WagmiConfig } from 'wagmi';
 import { publicProvider } from 'wagmi/providers/public'
 import './App.scss';
 import WalletLoginPage from './pages/WalletLoginPage';
-import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { createAuthenticationAdapter, getDefaultWallets, RainbowKitAuthenticationProvider, RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import '@rainbow-me/rainbowkit/styles.css';
+import { SiweMessage } from 'siwe';
 
 const { chains, provider, webSocketProvider } = configureChains(
   [chain.mainnet, chain.polygon],
@@ -15,6 +16,52 @@ const { connectors } = getDefaultWallets({
   appName: 'My RainbowKit App',
   chains
 });
+
+const authenticationAdapter = createAuthenticationAdapter({
+  getNonce: async () => {
+    // TODO nonce
+    // const response = await fetch('/api/nonce');
+    return '21'
+  },
+
+  createMessage: ({ nonce, address, chainId }) => {
+    return new SiweMessage({
+      domain: window.location.host,
+      address,
+      // TODO name
+      statement: 'Sign in with Ethereum to the app.',
+      uri: window.location.origin,
+      version: '1',
+      chainId,
+      nonce,
+    });
+  },
+
+  getMessageBody: ({ message }) => {
+    return message.prepareMessage();
+  },
+
+  verify: async ({ message, signature }) => {
+    // TODO verify
+
+    console.log('Veryfying', message, signature)
+
+    return true;
+    const verifyRes = await fetch('/api/verify', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ message, signature }),
+    });
+
+    return Boolean(verifyRes.ok);
+  },
+
+  signOut: async () => {
+    // TODO logout
+    await fetch('/api/logout');
+  },
+});
+
 
 const client = createClient({
   autoConnect: true,
@@ -27,14 +74,19 @@ const client = createClient({
 function App() {
   return (
     <WagmiConfig client={client}>
-      <RainbowKitProvider coolMode chains={chains}>
-        <div >
-          <header className="m-2">
-            EgoBlock
-          </header>
-          <WalletLoginPage />
-        </div>
-      </RainbowKitProvider>
+      <RainbowKitAuthenticationProvider
+        adapter={authenticationAdapter}
+        status={'unauthenticated'}
+      >
+        <RainbowKitProvider coolMode chains={chains} >
+          <div >
+            <header className="m-2">
+              EgoBlock
+            </header>
+            <WalletLoginPage />
+          </div>
+        </RainbowKitProvider>
+      </RainbowKitAuthenticationProvider>
     </WagmiConfig>
   );
 }
