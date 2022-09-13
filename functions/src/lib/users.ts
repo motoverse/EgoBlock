b81fec600fda64ce1401f39c928aad4dfc17fd35
Pavel Schoffer <xschof00@gmail.com>
2022-09-13 15:34:51 +0200

    use NFTs

--- functions/src/lib/users.ts
+++ functions/src/lib/users.ts
@@ -1,5 +1,52 @@
 export const getUserInfo = async (uid: string) => {
+    const wallet = await fetchNFTs(uid)
+
     return {
         uid,
+        token: wallet.tokens
+    }
+}
+
+interface GraphWallet {
+    id: string
+    tokens: GraphToken[]
+}
+
+interface GraphToken {
+    contractAddress: string
+    tokenId: string
+}
+const fetchNFTs = async (address: string): Promise<GraphWallet> => {
+    var axios = require('axios');
+    var data = JSON.stringify({
+        query: `{
+                    wallets(where: {id: "${address}"}) {
+                        id
+                        tokens {
+                        id
+                        }
+                    }
+                }`,
+        variables: {}
+    });
+
+    var config = {
+        method: 'post',
+        url: 'https://api.thegraph.com/subgraphs/name/pschoffer/egoblock',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        data: data
+    };
+
+    const response = await axios(config);
+
+    if (response?.data?.data?.wallets?.length > 0) {
+        return response.data.data.wallets[0];
+    }
+
+    return {
+        id: address,
+        tokens: []
     }
 }
